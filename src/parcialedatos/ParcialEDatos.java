8d998c793dd7119e0d259a2cacfc08fcaef2308b
Katheryn Granada R <Katheryn Granada R@DESKTOP-CO0IK4C>
2019-10-10 09:49:41 -0500

    Se sube el proyecto

--- src/parcialedatos/ParcialEDatos.java
+++ src/parcialedatos/ParcialEDatos.java
@@ -1,21 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package parcialedatos;
 
-/**
- *
- * @author Katheryn Granada R
- */
 public class ParcialEDatos {
+    
+    private int id;
+    private String fecha;
+    private String nombre;
+    private String commit;
+    private String push;
+    private String pull;
+    private String clase;
+    private String objeto;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCommit() {
+        return commit;
+    }
+
+    public void setCommit(String commit) {
+        this.commit = commit;
+    }
+
+    public String getPush() {
+        return push;
+    }
+
+    public void setPush(String push) {
+        this.push = push;
+    }
+
+    public String getPull() {
+        return pull;
+    }
+
+    public void setPull(String pull) {
+        this.pull = pull;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public String getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(String objeto) {
+        this.objeto = objeto;
     }
     
 }
+    
