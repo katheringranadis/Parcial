378bb2a0627809799150b2d8b6eba42cbdcc0ba2
Katheryn Granada R <Katheryn Granada R@DESKTOP-CO0IK4C>
2019-10-10 11:00:04 -0500

    Ultimo Cambio

--- src/parcialedatos/ParcialEDatos.java
+++ src/parcialedatos/ParcialEDatos.java
@@ -36,27 +36,27 @@ public class ParcialEDatos {
 
     //metodo commit
     public void commit(){
-        
+        JOptionPane.showMessageDialog(null,"Que es Commit: Aqui deberia ir mas informacion pero por tiempo, lo siento!");
     }
     
     //metodo push
     public void push(){
-        
+        JOptionPane.showMessageDialog(null,"Que es Push: Aqui deberia ir mas informacion pero por tiempo, lo siento!");
     }
     
     //metodo pull
     public void pull(){
-        
+        JOptionPane.showMessageDialog(null,"Que es Pull: Aqui deberia ir mas informacion pero por tiempo, lo siento!");
     }
     
     //metodo clase
     public void clase(){
-        
+        JOptionPane.showMessageDialog(null,"Que es una Clase: Aqui deberia ir mas informacion pero por tiempo, lo siento!");
     }
     
     //metodo objeto
     public void objeto(){
-        
+        JOptionPane.showMessageDialog(null,"Que es un objeto: Aqui deberia ir mas informacion pero por tiempo, lo siento!");
     }
    
 }
